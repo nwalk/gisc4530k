71906034b87706bb462a2f7172c973dc599f2c78
nrwalk8939 <nwalk19@gmail.com>
2014-10-02 14:31:06 -0400

    HTML, models, and admin files
    I have been working on the HTML layout i created using bootstrap
    I have added many of the models i will need for this API
    I have registered all my models in the admin

--- apps/university/admin.py
+++ apps/university/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from apps.university import models
 
-admin.site.register(models.University)
+admin.site.register(models.Students)
+admin.site.register(models.Instructors)
+admin.site.register(models.Courses)
+admin.site.register(models.Classes)
+admin.site.register(models.Locations)
--- apps/university/models.py
+++ apps/university/models.py
@@ -1,10 +1,54 @@
 from django.contrib.gis.db import models
 
-class University(models.Model):
-    """This is a model of no consequence"""
+
+class Students(models.Model):
+    """Model for student information"""
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    student_id = models.IntegerField(max_length=9)
+    student_major = models.CharField(max_length=50)
+    status = models.CharField(max_length=40)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class Instructors(models.Model):
+    """Model for instructor information"""
     name = models.CharField(max_length=40)
-    status = models.BooleanField()
-    desc = models.CharField(max_length=280)
+    status = models.CharField(max_length=40)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class Courses(models.Model):
+    """Model for course information"""
+    CRN = models.IntegerField(max_length=4)
+    section = models.IntegerField(max_length=2)
+    course = models.CharField(max_length=10)
+    session = models.TextField(max_length=15)
+    title = models.CharField(max_length=40)
+    hours = models.IntegerField(max_length=1)
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class Classes(models.Model):
+    """An instant of courses"""
+    course = models.CharField(max_length=40)
+    date_taken = models.DateField()
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class Locations(models.Model):
+    """Model for location information"""
+    name = models.CharField(max_length=40)
+    status = models.CharField(max_length=40)
 
     def __str__(self):
         return "{}".format(self.name)
